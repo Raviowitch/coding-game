2ec8d9803a4c6bc655377755de215007c3824bfc
Mathieu <lavergne.mat@gmail.com>
2022-02-09 19:43:46 +0100

    Simplification Brackets Extreme Edition

--- Easy Puzzles/Brackets Extreme Edition/index.js
+++ Easy Puzzles/Brackets Extreme Edition/index.js
@@ -1,46 +1,23 @@
 const expression = readline().split('')
-    .filter(val => ['(', '{', '[', ')', '}', ']'].includes(val))
-    .map(val => { return { value: val, side: ['(', '{', '['].includes(val) ? 'L' : 'R' } });
+    .filter(val => ['(', '{', '[', ')', '}', ']'].includes(val));
 
-displayResult(expression)
-
-function displayResult(expression) {
-    if (!checkEqualityBetweenLeftAndRight(expression)) {
-        console.log(false)
-    } else if (!checkIfLeftBeforeRight(expression)) {
-        console.log(false)
-    } else if (!checkIfThereIsNoError(expression)) {
-        console.log(false)
-    } else {
-        console.log(true)
-    }
-}
-
-function checkEqualityBetweenLeftAndRight(expression) {
-    var Ls = [...expression.filter(val => val.side === 'L')];
-    var Rs = [...expression.filter(val => val.side === 'R')];
-    return Ls.length === Rs.length;
-}
-
-function checkIfLeftBeforeRight(expression) {
-    return expression.findIndex(val => val.side === 'L') < expression.findIndex(val => val.side === 'R');
-}
+console.log(checkIfThereIsNoError(expression))
 
 function checkIfThereIsNoError(expression) {
     var values = [{ L: '(', R: ')' }, { L: '{', R: '}' }, { L: '[', R: ']' }];
     var result = true;
-    var nextRs = [];
-    var previousLs = [];
+    var rights = [];
+    var lefts = [];
     expression.forEach(val => {
-        if (val.side === 'L') {
-            nextRs.push(values.find(v => v.L === val.value).R);
-            previousLs.push(val.value);
-        } else if (val.side === 'R' && val.value === nextRs[nextRs.length - 1]) {
-            previousLs.pop();
-            nextRs.pop();
+        if (['(', '{', '['].includes(val)) {
+            rights.push(values.find(v => v.L === val).R);
+            lefts.push(val);
+        } else if ([')', '}', ']'].includes(val) && val === rights[rights.length - 1]) {
+            lefts.pop();
+            rights.pop();
         } else {
             result = false;
         }
     })
-    return result;
+    return result && lefts.length === 0 && rights.length === 0;
 }
