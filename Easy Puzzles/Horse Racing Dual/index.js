76f280dd7d23caffbc5808633a70871b0533c2cd
Mathieu <lavergne.mat@gmail.com>
2022-04-09 19:14:06 +0200

    Update Horse Racing Dual

--- Easy Puzzles/Horse Racing Dual/index.js
+++ Easy Puzzles/Horse Racing Dual/index.js
@@ -1,15 +1,10 @@
-const N = parseInt(readline());
-var horses = [];
-for (let i = 0; i < N; i++) {
-    const pi = parseInt(readline());
-    horses.push(pi);
-}
-horses.sort((a,b)=>{return a-b});
-var minGap = 10000001; // More than max horse power
-var previous = horses[0];
-for (let i = 1; i < horses.length; i ++) {
-    minGap = Math.min(minGap, horses[i]-previous);
-    previous = horses[i];
-}
-
+const N = +readline();
+var horses = [...Array(N)].map(_ => readline()).sort((a, b) => a - b);
+var minGap = Infinity;
+horses.forEach((horse, index) => {
+    if (index !== 0) {
+        minGap = Math.min(minGap, horse - previous);
+    }
+    previous = horse;
+})
 console.log(minGap);
