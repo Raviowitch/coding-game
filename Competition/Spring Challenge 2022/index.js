2d413ad5c1c6f8834495f13d9b91f0c092366c32
Mathieu <lavergne.mat@gmail.com>
2022-04-24 18:28:11 +0200

    Spring Challenge 2022 - Ligue Argent

--- Competition/Spring Challenge 2022/index.js
+++ Competition/Spring Challenge 2022/index.js
@@ -5,6 +5,7 @@ const heroesPerPlayer = parseInt(readline());
 
 const baseXOpponent = baseX === 0 ? 17630 : 0;
 const baseYOpponent = baseY === 0 ? 9000 : 0;
+var readyToBeAnAsshole = false;
 
 while (true) {
     let spiders = [];
@@ -40,7 +41,7 @@ while (true) {
         } else if (spiders[i].threatFor === 1) {
             threat_level = 200;
         }
-        let distanceBetweenBaseAndSpider = getDistance(spiders[i]);
+        let distanceBetweenBaseAndSpider = getDistance(baseX, baseY, spiders[i]);
         console.error(distanceBetweenBaseAndSpider);
         threat_level += (500 * (1 / (distanceBetweenBaseAndSpider + 1))) * 1000;
         sorted_spiders.push({ threat_level, spider: spiders[i] });
@@ -49,28 +50,36 @@ while (true) {
         return b.threat_level - a.threat_level;
     })
     for (let i = 0; i < heroesPerPlayer; i++) {
-        // Le premier héros est statique, il reste en défense et se contente de faire des spells WIND pour repousser les ennemis
-        // Strat pussy
         if (i === 0) {
             if (sorted_spiders.length > 0 && sorted_spiders[0].threat_level > 1200 && hero_infos_mana >= 10) {
                 console.log(`SPELL WIND ${baseXOpponent} ${baseYOpponent}`);
             } else {
                 console.log('WAIT');
             }
-        } else {
+        } else if (i === 1) {
             if (sorted_spiders.length > i) {
                 console.log(`MOVE ${sorted_spiders[i].spider.x} ${sorted_spiders[i].spider.y}`);
             } else {
-                if (sorted_spiders.length > 0) {
+                if (sorted_spiders.length > 0 && i <= 1) {
                     console.log(`MOVE ${sorted_spiders[0].spider.x} ${sorted_spiders[0].spider.y}`);
+                } else {
+                    moveInFrontOfBase(i)
+                }
+            }
+        } else {
+            if ((my_heroes[2].x === 14800 && my_heroes[2].y === 6200) || (my_heroes[2].x === 2150 && my_heroes[2].y === 2150)) {
+                readyToBeAnAsshole = true;
+            }
+            if (!readyToBeAnAsshole) {
+                moveInFrontOfEnnemyBase()
+            } else {
+                if (hero_infos_mana >= 10 && atLeastOneSpiderClose(my_heroes[2].x, my_heroes[2].y, spiders)) {
+                    console.log(`SPELL WIND ${baseXOpponent} ${baseYOpponent}`);
                 } else {
                     console.log('WAIT');
                 }
             }
         }
-
-
-
         // In the first league: MOVE <x> <y> | WAIT; In later leagues: | SPELL <spellParams>;
     }
 }
@@ -91,6 +100,43 @@ function createEntity(inputs) {
     return { id, type, x, y, shieldLife, isControlled, health, vx, vy, nearBase, threatFor }
 }
 
-function getDistance(spider) {
-    return Math.hypot((baseX - spider.x), (baseY - spider.y))
+function getDistance(x, y, spider) {
+    return Math.hypot((x - spider.x), (y - spider.y))
+}
+
+function moveInFrontOfBase(i) {
+    if (baseX === 0) {
+        // if (i === 0) {
+        //     console.log(`MOVE 6500 1500`);
+        // } else if (i === 1) {
+        console.log(`MOVE 4500 4200`);
+        // } if (i === 2) {
+        //     console.log(`MOVE 6500 3000`);
+        // }
+    } else {
+        // if (i === 0) {
+        //     console.log(`MOVE 10500 7000`);
+        // } else if (i === 1) {
+        console.log(`MOVE 12200 5200`);
+        // } if (i === 2) {
+        //     console.log(`MOVE 14000 2500`);
+        // }
+    }
+}
+
+function moveInFrontOfEnnemyBase() {
+    if (baseX === 0) {
+        console.log(`MOVE 14800 6200`);
+    } else {
+        console.log(`MOVE 2150 2150`);
+    }
+}
+
+function atLeastOneSpiderClose(x, y, spiders) {
+    for (let i = 0; i < spiders.length; i++) {
+        if(getDistance(x, y, spiders[i]) <= 1280) {
+            return true;
+        }
+    }
+    return false;
 }
